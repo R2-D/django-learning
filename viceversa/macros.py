957654c333a10642a4d960042e9b7cab7d79540e
R2-D <64431968+R2-D@users.noreply.github.com>
2020-11-19 19:58:37 +0200

    add no_of_words
    Add count words in original text

--- viceversa/macros.py
+++ viceversa/macros.py
@@ -7,4 +7,5 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext': user_text, 'reversed_text': reversed_text})
+	count_words = len(user_text.split())
+	return render(request, 'reverse.html', {'usertext': user_text, 'reversed_text': reversed_text, 'no_of_words': count_words})
